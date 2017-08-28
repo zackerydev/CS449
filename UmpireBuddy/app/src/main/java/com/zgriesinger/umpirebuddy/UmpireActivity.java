0beaef27f4f431739f19dcd9de36e3642f744a62
Zackery Griesinger <zgriesinger@gmail.com>
2017-08-28 18:16:25 -0500

    Initial Commit: UmpireBuddy Project Creation
    Initializing the repository with the basis for the UmpireBuddy application as an Android Studio project with the default empty activity view

---
+++ UmpireBuddy/app/src/main/java/com/zgriesinger/umpirebuddy/UmpireActivity.java
@@ -0,0 +1,13 @@
+package com.zgriesinger.umpirebuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UmpireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umpire);
+    }
+}
