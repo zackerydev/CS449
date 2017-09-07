25bfd27d5042fe8ffca8cce1673214cf28da3128
Zackery Griesinger <zgriesinger@gmail.com>
2017-09-07 07:11:45 -0500

    Added: Persistant Out Counter, About and Refresh Menu
    TODO: Contextual menu on hold
    Persist count over rotation

--- UmpireBuddy/app/src/main/java/com/zgriesinger/umpirebuddy/UmpireActivity.java
+++ UmpireBuddy/app/src/main/java/com/zgriesinger/umpirebuddy/UmpireActivity.java
@@ -1,23 +1,68 @@
 package com.zgriesinger.umpirebuddy;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.Menu;
+
+import org.w3c.dom.Text;
 
 public class UmpireActivity extends AppCompatActivity {
     private Button mStrikeButton;
     private Button mBallButton;
+    public static final String MY_PREFS_NAME = "DataFile";
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public void showAbout() {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if(item.getItemId() == R.id.restart) {
+            resetCount();
+            return true;
+        } else if (item.getItemId() == R.id.about) {
+            showAbout();
+            return true;
+        }
+        return true;
+    }
+
+    public void resetCount() {
+        final TextView strikeCount = (TextView) findViewById(R.id.strike_counter);
+        final TextView ballCount = (TextView) findViewById(R.id.ball_counter);
+
+        ballCount.setText("0");
+        strikeCount.setText("0");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         //Basic setup to initialize the app, as well as global TextViews to update the text from
         // the anonymous inner classes
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_umpire);
         final TextView strikeCount = (TextView) findViewById(R.id.strike_counter);
         final TextView ballCount = (TextView) findViewById(R.id.ball_counter);
+        final TextView outCount = (TextView) findViewById(R.id.out_counter);
+        SharedPreferences countData = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String outs = countData.getString("outs", "0");
+        outCount.setText(outs);
+
+
+
 
         mBallButton = (Button) findViewById(R.id.ball_btn);
         mBallButton.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +96,13 @@ public class UmpireActivity extends AppCompatActivity {
                     String newCount = Integer.toString(strikeInt);
                     strikeCount.setText(newCount);
                 } else {
+                    int outInt = Integer.parseInt((String) outCount.getText());
+                    outInt++;
+                    String newOuts = Integer.toString(outInt);
+                    SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                    editor.putString("outs", newOuts);
+                    editor.apply();
+                    outCount.setText(newOuts);
                     Toast.makeText(UmpireActivity.this, R.string.strikeout_toast, Toast.LENGTH_SHORT).show();
                     strikeCount.setText("0");
                     ballCount.setText("0");
@@ -61,4 +113,5 @@ public class UmpireActivity extends AppCompatActivity {
 
 
     }
+
 }
